f6915e6c5be3564e605544be99894b2e4da7673b
Radu Woinaroski <radu@sentry.io>
2019-06-25 18:02:36 +0200

    simplified tests

--- sentry/src/test/kotlin/io/sentry/SerializationTest.kt
+++ sentry/src/test/kotlin/io/sentry/SerializationTest.kt
@@ -1,55 +1,68 @@
 package io.sentry
 
 import com.google.gson.JsonObject
+import com.google.gson.JsonParser
+import com.google.gson.JsonPrimitive
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 /**
- *  returns a regex for checking for a property serialized in json
+ * Generic accessor for tests
  */
-private fun getSerializedPropRegex(propName: String, propVal: String):Regex{
-   return Regex(""""$propName"\s*:\s*"$propVal"""")
+private fun getValue(theObj: JsonObject, vararg path: String): Any {
+    if (path.isEmpty()) {
+        throw Exception("null path in getValue")
+    }
+    val currentPath = path[0]
+
+    val element = theObj.get(currentPath) ?: throw Exception("could not find member at path $currentPath")
+
+    if (path.size == 1) {
+        if (element is JsonPrimitive){
+            when {
+                element.isBoolean -> return element.asBoolean
+                element.isNumber -> return element.asNumber
+                element.isString -> return element.asString
+                else -> throw Exception("unsupported element type ${element.javaClass.name}")
+            }
+        }
+        throw Exception("getValue can only navigate to primitive types")
+
+    } else {
+        if (element is JsonObject)
+            return getValue(element, *path.drop(1).toTypedArray())
+        throw Exception("expecting an object but got ${element.javaClass.name} at element $currentPath")
+    }
 }
 
-/**
- * Generic deserialization
- */
-//private fun getValue(theObj: JsonObject, vararg path: String):Any{
-//    if ( path.isEmpty()){
-//        return theObj
-//    }
-//    val currentPath = path[0]
-//
-//    if ( path.size == 1){
-//
-//    }
-//}
-
+private fun stringToJsonObj( serialized: String): JsonObject {
+    return JsonParser().parse(serialized).asJsonObject
+}
 
 
 class SentrySerializationTest {
     @Test
-    fun `should serialize a simple event`(){
+    fun `should serialize a simple event`() {
         val evt = SentryEvent()
-
         val eventString = serializeEvent(evt)
-        // TODO Rwn. remove
-        println(eventString)
-        val idRegex = getSerializedPropRegex("event_id", evt.eventId)
-        assertTrue(idRegex.containsMatchIn(eventString),"could not find the eventId" )
-        val timeStampRegex = getSerializedPropRegex("time_stamp", evt.timestamp)
-        assertTrue(timeStampRegex.containsMatchIn(eventString),"could not find the timestamp" )
+        val jsonObj = stringToJsonObj(eventString)
+
+        assertEquals(evt.eventId, getValue(jsonObj, "event_id"))
+        assertEquals(evt.timestamp, getValue(jsonObj, "time_stamp"))
     }
 
     @Test
-    fun `should serialize module elements`(){
+    fun `should serialize module elements`() {
         val evt = SentryEvent()
         evt.modules["module1"] = "hello"
         evt.modules["module2"] = "world"
 
         val eventString = serializeEvent(evt)
-        println(eventString)
+        val jsonObj = stringToJsonObj(eventString)
 
+        assertEquals("hello",  getValue(jsonObj,"modules", "module1"))
+        assertEquals("world",  getValue(jsonObj,"modules", "module2"))
 
     }
 
